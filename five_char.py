ef737708fecb5c08b68068e1d14f7e0272703f31
dante <dbollono@gmail.com>
2026-03-27 21:12:11 -0300

    changes some things

--- five_char.py
+++ five_char.py
@@ -4,10 +4,10 @@ import os
 att = 0
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
-ALF = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6","7","8","9"
+ALF = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 ACH = False
 len5 = False
-while len5 == False:
+while not len5:
     ANS = input("Please input any 5 character combination  > ")
     length = len(ANS)
     if length == 5:
@@ -28,5 +28,4 @@ while ACH == False:
     print(char, str(att))
     if char == ANS:
         print("Success! It took",str(att),"attempts to get your 5 character combination")
-    else:
-        pass
+        ACH = True
